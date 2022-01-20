565b051a0a42a773dfaa7e49103fdb1b35137fa7
1Silver23 <80821281+1Silver23@users.noreply.github.com>
2022-01-20 20:57:23 +0300

    new solutions

---
+++ GeekBraineslessons/MinMax numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, min;
+        Console.WriteLine("Введите два целых числа.");
+        Console.Write("Введите первое число:");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число:");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        if (a < b)
+        {
+            min = b;
+        }
+        else
+        {
+            min = a;
+        };
+        Console.WriteLine("Наибольшее число {0}", min);
+        Console.WriteLine("Для выхода из приложение нажмите Enter");
+        Console.ReadLine();
+    }
+}
---
+++ GeekBraineslessons/body volume/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Привет___рост__вес_и_объёим_тела
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветствую!");
+            Console.Write("Как вас зовут?");
+            String name = Console.ReadLine();
+            Console.Write("Какая у вас фамилия?");
+            String f = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            String e = Console.ReadLine();
+            Console.Write("Ваш рост?");
+            String b = Console.ReadLine();
+            Console.Write("Сколько вы весите?");
+            String w = Console.ReadLine();
+            int i = Convert.ToInt32(w) / (Convert.ToInt32(b) * Convert.ToInt32(b));   //не получается формулой расчитать кого во что  нужно  конвертировать?
+            Console.WriteLine($"рад с вами познакомиться {name} {f}");
+            Console.WriteLine($"Ваш рост {b}, вес {w}, возраст {e} и объём тела {i}");
+            Console.ReadKey();
+        }
+    }
+}
