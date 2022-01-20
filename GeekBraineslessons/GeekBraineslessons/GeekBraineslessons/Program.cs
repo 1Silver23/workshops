f4dfb2b250d9ef8ad82e3ea41177617e3ec5b097
1Silver23 <80821281+1Silver23@users.noreply.github.com>
2022-01-20 11:36:18 +0300

    sum result output

---
+++ GeekBraineslessons/GeekBraineslessons/GeekBraineslessons/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBraineslessons
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;            
+            double D = a + (b * c);
+            Console.WriteLine($"буква первая {a},\n буква вторая {b},\n буква третия {c}\n и буква четвёртая {D}\n");
+            Console.ReadKey();
+        }
+    }
+}
